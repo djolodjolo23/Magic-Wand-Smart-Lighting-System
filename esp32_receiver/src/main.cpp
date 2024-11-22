77c7b776f3ac4016d5c24788ac48f686ef2132cb
Djordje Dimitrov <dimitrovdjordje@gmail.com>
2024-11-22 10:44:33 +0100

    Added the Bluetooth connection code.

--- esp32_receiver/src/main.cpp
+++ esp32_receiver/src/main.cpp
@@ -1,18 +1,76 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
 
-// put function declarations here:
-int myFunction(int, int);
+static BLEUUID serviceUUID("18cfb27c-df3d-41c7-801b-60165e9c9872");
+static BLEUUID charUUID("78eb176c-cef1-4295-8a5f-69923ca804b9");
+
+BLEClient* client;
+BLERemoteCharacteristic* remoteChar;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  BLEDevice::init("ESP32_Central");
+  client = BLEDevice::createClient();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!client->isConnected()) {
+    Serial.println("Not connected, scanning for devices..."); // attempt to connect to NanoBLE
+    BLEScan* scan = BLEDevice::getScan();
+    scan->setActiveScan(true); // active scan uses more power, but get results faster
+    BLEScanResults results = scan->start(5);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    bool foundDevice = false;
+
+    for (int i = 0; i < results.getCount(); i++) {
+      BLEAdvertisedDevice device = results.getDevice(i);
+      Serial.print("Found Device: ");
+      Serial.println(device.getName().c_str());
+
+      if (device.getName() == "NanoBLE") { // NanoBLE is the name of the peripheral device, if found, connect to it
+        Serial.println("Found NanoBLE, attempting to connect...");
+        client->connect(&device);
+        foundDevice = true;
+        break;
+      }
+    }
+
+    if (foundDevice && client->isConnected()) {
+      Serial.println("Connected to NanoBLE");
+
+      BLERemoteService* remoteService = client->getService(serviceUUID);
+      if (remoteService == nullptr) {
+        Serial.println("Failed to find our service UUID");
+        client->disconnect();
+        return;
+      }
+
+      remoteChar = remoteService->getCharacteristic(charUUID);
+      if (remoteChar == nullptr) {
+        Serial.println("Failed to find our characteristic UUID");
+        client->disconnect();
+        return;
+      }
+      Serial.println("Found characteristic");
+    } else {
+      Serial.println("Failed to connect to NanoBLE");
+    }
+
+    scan->clearResults(); 
+    delay(1000);
+
+  } else {
+    if (remoteChar && remoteChar->canRead()) {
+      uint8_t value = remoteChar->readUInt8();
+      Serial.print("Received: ");
+      Serial.println(value);
+    } else {
+      Serial.println("Cannot read from characteristic");
+    }
+
+    if (!client->isConnected()) {
+      Serial.println("Disconnected from NanoBLE");
+    }
+  }
 }
--- nano_wand/src/main.cpp
+++ nano_wand/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BLEService numberService("18cfb27c-df3d-41c7-801b-60165e9c9872");
+BLEUnsignedCharCharacteristic numberCharacteristic("78eb176c-cef1-4295-8a5f-69923ca804b9", BLERead | BLEWrite);
+
+u_int8_t num = 1;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("NanoBLE");
+  BLE.setAdvertisedService(numberService);
+  numberService.addCharacteristic(numberCharacteristic);
+  BLE.addService(numberService);
+
+  numberCharacteristic.writeValue(num);
+
+  BLE.advertise();
+
+  Serial.println("BLE LED Peripheral");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  BLEDevice central = BLE.central(); // nano is peripheral since transmitting, esp32 is central, find the central device
+  if (central) { // if central device is found
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+    while (central.connected()) {
+      numberCharacteristic.writeValue(num);
+      num = (num % 10) + 1; // for now I just want to cycle through 1-10 for testing
+      delay(1000);
+    }
+    Serial.print("Disconnected from central: ");
+  }
+  Serial.println("Not connected...");
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}

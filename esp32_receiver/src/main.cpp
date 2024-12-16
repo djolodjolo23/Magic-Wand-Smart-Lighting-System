c2df37b50a0a1b5aad581fecb90fdb9907110225
Djordje Dimitrov <dimitrovdjordje@gmail.com>
2024-12-16 16:30:21 +0100

    Updated the config file for new esp32.

--- esp32_receiver/src/main.cpp
+++ esp32_receiver/src/main.cpp
@@ -6,14 +6,17 @@ static const char* sUUID = "18cfb27c-df3d-41c7-801b-60165e9c9872";
 static const char* cUUID = "78eb176c-cef1-4295-8a5f-69923ca804b9";
 
 BluetoothCentral btCentral(sUUID, cUUID, "ESP32_Central");
-IRReceive irReceiver(3, 2);
+IRReceive irReceiver(32, 33);
 
 void setup() {
   Serial.begin(9600);
+  delay(2000);
+  Serial.println("Starting...");
   //BLEDevice::init("ESP32_Central");
 }
 
 void loop() {
+  
   /*
   unsigned long previousMillis = millis();
   const long interval = 1000;
--- nano_wand/src/main.cpp
+++ nano_wand/src/main.cpp
@@ -27,23 +27,23 @@ void setup() {
 }
 
 void loop() {
-    long previousMillis = millis(); 
-    const long interval = 1000;     
-    while (!btPeripheral.isConnectedToCentral()) {
-        if (millis() - previousMillis >= interval) {
-            previousMillis = millis();
-            Serial.println("Waiting for connection...");
-        }
-    }
-    btPeripheral.updateValue(value);
+    // long previousMillis = millis(); 
+    // const long interval = 1000;     
+    // while (!btPeripheral.isConnectedToCentral()) {
+    //     if (millis() - previousMillis >= interval) {
+    //         previousMillis = millis();
+    //         Serial.println("Waiting for connection...");
+    //     }
+    // }
+    // btPeripheral.updateValue(value);
 
-    Serial.print("Updating value to: ");
-    Serial.println(value);
+    // Serial.print("Updating value to: ");
+    // Serial.println(value);
 
-    value = (value % 10) + 1;
-    delay(1000);
+    // value = (value % 10) + 1;
+    // delay(1000);
 
-    //irControl.update();
+    irControl.update();
 
     //String motion = motionHandler.processMotion();
     //if (motion != "") {

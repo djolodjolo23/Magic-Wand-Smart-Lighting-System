afd3b3358f59e062edcb2f2ce8110f021fa5c7b2
Djordje Dimitrov <dimitrovdjordje@gmail.com>
2024-12-16 08:52:25 +0100

    Added the IR signal receiving class on esp32.

--- esp32_receiver/src/main.cpp
+++ esp32_receiver/src/main.cpp
@@ -1,24 +1,34 @@
 #include <Arduino.h>
 #include "BluetoothCentral.hpp"
+#include "IRReceive.hpp"
 
 static const char* sUUID = "18cfb27c-df3d-41c7-801b-60165e9c9872";
 static const char* cUUID = "78eb176c-cef1-4295-8a5f-69923ca804b9";
 
 BluetoothCentral btCentral(sUUID, cUUID, "ESP32_Central");
+IRReceive irReceiver(3, 2);
 
 void setup() {
   Serial.begin(9600);
-  BLEDevice::init("ESP32_Central");
+  //BLEDevice::init("ESP32_Central");
 }
 
 void loop() {
-  if (!btCentral.isConnected()) {
-    btCentral.connect();
-    delay(1000);
-  } else {
-    uint8_t value = btCentral.readValue();
-    Serial.print("Received: ");
-    Serial.println(value);
-    delay(1000);
+  /*
+  unsigned long previousMillis = millis();
+  const long interval = 1000;
+  while (!btCentral.isConnected()) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      Serial.println("Attempting to connect...");
+      btCentral.connect();
+      previousMillis = currentMillis;
+    }
   }
+  Serial.println("Connected!");
+  //uint8_t val = btCentral.readValue();
+  //Serial.println(val);
+  */
+  irReceiver.listenForIR();
+  
 }
